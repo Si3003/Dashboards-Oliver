7ae6fce574af839a8098922a01fc45496cdd8b05
SiLaek <110716793+Si3003@users.noreply.github.com>
2024-05-24 13:42:10 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -105,6 +105,14 @@ const files = {
         {
             name: 'PATCHING_108',
             url: 'https://mapcoid-my.sharepoint.com/personal/oliver_panjaitan_map_co_id/_layouts/15/download.aspx?UniqueId=bba200bf%2Df650%2D47c8%2Dbdb4%2Dc0e224e1cfef'
+        },
+        {
+            name: 'NULL',
+            url: 'https://example.com/path/to/DatabaseSuite_Y2.zip'
+        },
+        {
+            name: 'NULL-',
+            url: 'https://example.com/path/to/DatabaseSuite_Z3.zip'
         }
     ],
     howto: [
